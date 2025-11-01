66138596699d964f0b1d7e0ad4d6c389b9399de6
Mike Weeks <mike@localhost.localdomain>
2025-10-31 21:08:40 -0400

    chore: Update tests

--- uv_lock_report/tests/conftest.py
+++ uv_lock_report/tests/conftest.py
@@ -1,20 +1,16 @@
 from uv_lock_report.models import LockfilePackage, OutputFormat, UpdatedPackage
 
 ADDED_PACKAGES: list[LockfilePackage] = [
-    LockfilePackage(name="added_1", version=Version(1, 0, 0)),
-    LockfilePackage(name="added_2", version=Version(4, 2, 0)),
+    LockfilePackage(name="added_1", version="1.0.0"),
+    LockfilePackage(name="added_2", version="4.2.0"),
 ]
 REMOVED_PACKAGES: list[LockfilePackage] = [
-    LockfilePackage(name="removed_1", version=Version(1, 0, 0)),
-    LockfilePackage(name="removed_2", version=Version(4, 2, 0)),
+    LockfilePackage(name="removed_1", version="1.0.0"),
+    LockfilePackage(name="removed_2", version="4.2.0"),
 ]
 UPDATED_PACKAGES: list[UpdatedPackage] = [
-    UpdatedPackage(
-        name="updated_1", old_version=Version(1, 0, 0), new_version=Version(2, 0, 0)
-    ),
-    UpdatedPackage(
-        name="updated_2", old_version=Version(1, 0, 0), new_version=Version(2, 0, 0)
-    ),
+    UpdatedPackage(name="updated_1", old_version="1.0.0", new_version="2.0.0"),
+    UpdatedPackage(name="updated_2", old_version="1.0.0", new_version="2.0.0"),
 ]
 
 EXPECTED_LOCKFILE_CHANGES_FULL_TABLE = """
